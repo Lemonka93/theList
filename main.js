36b15ccaf89e3dc75b75d7643b75bec2e4faa6eb
Lemonka93 <lerayepishko@gmail.com>
2021-01-08 02:34:24 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+// console.log(localStorage.getItem("notes"));
+const App = {
+  data() {
+    return {
+      message: "Новогодний дожор:",
+      inputPlaceholder: "Ввведите блюдо",
+      inputValue: "",
+      btnAdd: "Добавить",
+      btnRemove: "Уничтожить доказательство дожора",
+      notes: JSON.parse(localStorage.getItem("notes")),
+    };
+  },
+  methods: {
+    inputChangeHandler(event) {
+      this.inputValue = event.target.value;
+    },
+    addNewNote() {
+      this.notes.push(this.inputValue);
+      localStorage.setItem("notes", JSON.stringify(this.notes));
+      this.inputValue = "";
+    },
+    removeNote(index) {
+      this.notes.splice(index, 1);
+      localStorage.setItem("notes", JSON.stringify(this.notes));
+      // console.log(localStorage.getItem("notes"));
+    },
+  },
+};
+
+Vue.createApp(App).mount("#app");
